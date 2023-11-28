b23a7110c234cbd24f426d8e6bc4b687d94acc49
qianlang95 <74698914+qianlang95@users.noreply.github.com>
2023-11-27 22:12:20 -0800

    dummy push

--- usedCarBidding/urls.py
+++ usedCarBidding/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path("dbconnection/", views.check_db_connection, name="check_db_connection"),
     path('', views.testmysql),
+
+
+
+
+
+
     path('detail/', views.car_detail),
     path('detailSeller/', views.car_detail_seller),
     path('bidding/', views.place_bid),
